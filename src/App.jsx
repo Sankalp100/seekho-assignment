fbe7ae9c83e6a927796a65d2a2e94e5b4f7ab597
Sankalp100 <sankalpgour123@gmail.com>
2023-03-03 04:30:17 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Card, Row, Col, Button, Radio, Space } from "antd";
+import { Card, Row, Col, Button, Radio, Space, Layout } from "antd";
 import "./styles.css";
 import SubmitPage from "./SubmitPage";
 import questionLogo from "./assets/questionImg.png";
 import reviewLogo from "./assets/review.webp";
+import data from './data'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,28 +13,7 @@ function App() {
   const [correctAnswer, setCorrectAnswer] = useState();
   const [score, setScore] = useState(0);
 
-  const data = [
-    {
-      id: 1,
-      question: "Q1: A line which cuts a pair of parallel lines is called",
-      options: ["Tangent", "Chord", "Traversal", "Intersector"],
-      ans: "Traversal",
-    },
-    {
-      id: 2,
-      question:
-        "Q2: If a certain sum of money can become 5 times of its principal in 10 years, then the rate of interest is",
-      options: ["20%", "30%", "40%", "50%"],
-      ans: "20%",
-    },
-    {
-      id: 3,
-      question:
-        "Q3: A shopkeeper purchases 15 mangoes for Rs. 10 and sells them at 10 mangoes for Rs. 15. Thus, he earns a profit of",
-      options: ["50%", "75%", "80%", "125%"],
-      ans: "125%",
-    },
-  ];
+  
 
   const onSelectOption = (e) => {
     let arr = [...answers];
@@ -55,6 +35,8 @@ function App() {
   };
 
   return (
+    <>
+    <div className="topNav">Frontend Assignment</div>
     <div style={{ padding: "44px" }}>
       {submit ? (
         <Card>
@@ -65,7 +47,7 @@ function App() {
           <Row justify="space-between">
             <Col span={2}></Col>
             <Col span={6}>
-              <Card style={{ height: "70vh", margin: "24px" }}>
+              <Card className="cardStyles">
                 <div style={{ textAlign: "center" }}>
                   <img src={reviewLogo} alt="logo" height="60" />
                 </div>
@@ -80,7 +62,7 @@ function App() {
               </Card>
             </Col>
             <Col span={14}>
-              <Card style={{ height: "70vh", width: "100%", margin: "24px" }}>
+              <Card className="cardStyles">
                 <div style={{ textAlign: "center" }}>
                   <img src={questionLogo} alt="logo" height="60" />
                 </div>
@@ -136,6 +118,7 @@ function App() {
         </>
       )}
     </div>
+    </>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    question: "Q1: A line which cuts a pair of parallel lines is called",
+    options: ["Tangent", "Chord", "Traversal", "Intersector"],
+    ans: "Traversal",
+  },
+  {
+    id: 2,
+    question:
+      "Q2: If a certain sum of money can become 5 times of its principal in 10 years, then the rate of interest is",
+    options: ["20%", "30%", "40%", "50%"],
+    ans: "20%",
+  },
+  {
+    id: 3,
+    question:
+      "Q3: A shopkeeper purchases 15 mangoes for Rs. 10 and sells them at 10 mangoes for Rs. 15. Thus, he earns a profit of",
+    options: ["50%", "75%", "80%", "125%"],
+    ans: "125%",
+  },
+];
+
+export default data;
